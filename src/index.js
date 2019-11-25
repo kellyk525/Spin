5b52ab1fcf0673c8ec5a0a98664d456b570e5ebb
Kelly <kjk224@cornell.edu>
2019-11-25 14:52:44 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import _ from 'lodash';
+import './style.css';
+import Icon from './harvest.png';
+
+function component() {
+    const element = document.createElement('div');
+
+    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add("hello");
+    const myIcon = new Image();
+    myIcon.src = Icon;
+    
+    element.appendChild(myIcon);
+    return element;
+}
+
+document.body.appendChild(component());
